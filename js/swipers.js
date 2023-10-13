d7a4622c89cce20977b580cf0e7d400eb9814c9c
snakiso <igorchagov@mail.ru>
2023-10-13 11:54:47 +0300

    add form, and two sliders

--- js/swipers.js
+++ js/swipers.js
@@ -1,5 +1,5 @@
 
-const swiper = new Swiper('.', {
+const swiper = new Swiper('.catalog-slider__slider', {
     // Optional parameters
     loop: false,
     spaceBetween: 30,
@@ -14,9 +14,32 @@ const swiper = new Swiper('.', {
     },
     // If we need pagination
     navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+        nextEl: '.catalog-slider__next',
+        prevEl: '.catalog-slider__prev',
     },
 
 });
 
+let swiper1 = undefined;
+
+function initSwiperCategory() {
+    var screenWidth = window.innerWidth;
+    if (screenWidth < 700 && swiper1 == undefined) {
+        swiper1 = new Swiper('.news__slider', {
+            loop: true,
+            slidesPerView: 1,
+            spaceBetween: 30,
+        });
+    } else if (screenWidth >= 700 && swiper1 !== undefined) {
+        swiper1.destroy();
+        swiper1 = undefined;
+    }
+}
+
+$(document).ready(function () {
+    initSwiperCategory();
+}); //Проверка при загрузке страницы
+
+$(window).on('resize', function () {
+    initSwiperCategory();
+}); //Запуск функции при изменении размера
