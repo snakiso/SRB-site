81f558634a9b21a85570f5555fbc2a65bac4511c
snakiso <igorchagov@mail.ru>
2023-12-07 16:40:13 +0300

    some chagne on animations

--- js/video.js
+++ js/video.js
@@ -3,8 +3,10 @@
 const video = document.querySelector('.back-video');
 const darkWall = document.querySelector('.back-video__dark-wall');
 const playBtn = document.querySelector('.hero__play');
+const hero = document.querySelector('.hero')
 
 const changeStyle = () => {
+    hero.classList.add('ready')
     video.style.width = '100%';
     video.style.height = '100%';
     video.style.position = 'absolute';
@@ -16,11 +18,13 @@ window.addEventListener('scroll', () => changeStyle());
 
 video.addEventListener('click', () => changeStyle());
 
-
-playBtn.addEventListener('click', () => {
-    video.style.width = '100vw';
-    video.style.height = '100vh';
-    video.style.position = 'fixed';
-    video.style.zIndex = '25'
-    darkWall.style.zIndex = '-1'
-})
+if (playBtn) {
+    playBtn.addEventListener('click', () => {
+        hero.classList.remove('ready')
+        video.style.width = '100vw';
+        video.style.height = '100vh';
+        video.style.position = 'fixed';
+        video.style.zIndex = '25'
+        darkWall.style.zIndex = '-1'
+    })
+}
